20f81ba2ed67a314445f53c544cb3a15a5c3cab2
Diogoribeiro11 <110627684+Diogoribeiro11@users.noreply.github.com>
2022-11-28 15:24:04 +0000

    Create Jumbotron.js

---
+++ src/js/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumbotron = () => {
+
+    return (
+        <div className="jumbotron">
+            <h1>A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet repellendus reiciendis! Eligendi possimus beatae consequatur, cupiditate et ipsum fugiat repudiandae architecto veritatis dignissimos! Eos repellendus molestiae quo excepturi modi.</p>
+            <button type="button" class="btn btn-primary">Call to action!</button>
+        </div>
+    );
+}
+
+export default Jumbotron;
